d46bfcd91636d675924291cc6147a376517b196d
Loeung sakhak <loeungsakhak438@gmail.com>
2026-01-24 13:09:41 +0700

    commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-black">
+        <h1 className="text-6xl font-bold text-blue-600 text-center">
+          Hi everyone, nice to meet you
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
